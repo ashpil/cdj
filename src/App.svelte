8e4d01b3e2531bfa49263e0e77cdc584fd8d07ac
ashpil <shpilenok2001@gmail.com>
2020-11-09 17:52:56 -0500

    cleans up css

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,26 @@
   import { routes } from "../.routify/routes";
 </script>
 
-<style  global>
-  @import "../static/global.css";
+<style global>
+  body {
+      margin: 0;
+      box-sizing: border-box;
+      font-family: sans-serif;
+  }
+
+  a {
+      color: rgb(0,100,200);
+      text-decoration: none;
+  }
+
+  a:hover {
+      text-decoration: underline;
+  }
+
+  img {
+    max-width: 100%;
+  }
 </style>
 
 <Router {routes} />
+
--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,14 +1,7 @@
 <style global>
-  body {
-    padding: 0;
-  }
   main {
     margin: 0 auto;
     max-width: $max-width;
-  }
-
-  img {
-    max-width: 100%;
   }
 </style>
 
