8ad8503bfb9c45b0b7dee76e8078dccf91601333
ashpil <shpilenok2001@gmail.com>
2020-12-27 16:51:47 +0300

    updates elegant geometry

---
+++ src/pages/_components/Katex.svelte
@@ -0,0 +1,19 @@
+<script>
+  import katex from 'katex';
+  import { onMount } from 'svelte';
+
+  export let displayMode = false;
+
+  let wrapper;
+
+  onMount(() => {
+    wrapper.innerHTML = katex.renderToString(wrapper.innerHTML, { displayMode });
+  });
+</script>
+
+<svelte:head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css">
+</svelte:head>
+
+<span bind:this={wrapper}><slot /></span>
+
