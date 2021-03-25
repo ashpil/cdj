f09749c39e18896ac9ab33102722767f206b9de7
ashpil <shpilenok2001@gmail.com>
2021-03-25 11:57:28 -0400

    fixes formatting

--- src/pages/_components/Intro.svelte
+++ src/pages/_components/Intro.svelte
@@ -1,13 +1,13 @@
 <script>
-  import { isActive, url } from "@roxi/routify";
-  import { slide } from "svelte/transition";
+  import { isActive, url } from '@roxi/routify';
+  import { slide } from 'svelte/transition';
 
   export let blurb;
 
   const links = [
-    ["/about", "about"],
-    ["/get_involved", "get involved"],
-    //['/learn', 'learn'],
+    ['/about', 'about'],
+    ['/get_involved', 'get involved'],
+    // ['/learn', 'learn'],
   ];
 
   let windowWidth;
@@ -239,13 +239,9 @@
           on:click={() => (opened = !opened)}
           class:opened>
           <svg width="60" height="60" viewBox="0 0 100 100">
-            <path
-              class="line line1"
-              d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
+            <path class="line line1" d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
             <path class="line line2" d="M 20,50 H 80" />
-            <path
-              class="line line3"
-              d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
+            <path class="line line3" d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
           </svg>
         </button>
       </div>
@@ -282,20 +278,19 @@
   </nav>
   {#if windowWidth > 950}
     <div id="socials-top">
-      <a href="https://www.instagram.com/cornelldatajournal/">
+      <a href="https://www.instagram.com/cornelldatajournal/" target="_blank">
         <img src="/socials/instagram.svg" alt="Instagram" />
       </a>
-      <a href="https://www.facebook.com/cornelldatajournal/">
+      <a href="https://www.facebook.com/cornelldatajournal/" target="_blank">
         <img src="/socials/facebook.svg" alt="Facebook" />
       </a>
-      <a
-        href="https://join.slack.com/t/cornell-data-journal/shared_invite/zt-mb391cgl-yqsPnKf6IptzobNInf~y0Q">
+      <a href="https://join.slack.com/t/cornell-data-journal/shared_invite/zt-mb391cgl-yqsPnKf6IptzobNInf~y0Q" target="_blank">
         <img src="/socials/slack.svg" alt="Slack" />
       </a>
-      <a href="https://www.linkedin.com/company/cornell-data-journal/">
+      <a href="https://www.linkedin.com/company/cornell-data-journal/" target="_blank">
         <img src="/socials/linkedin.svg" alt="LinkedIn" />
       </a>
-      <a href="https://twitter.com/c_datajournal">
+      <a href="https://twitter.com/c_datajournal" target="_blank">
         <img src="/socials/twitter.svg" alt="Twitter" />
       </a>
     </div>
