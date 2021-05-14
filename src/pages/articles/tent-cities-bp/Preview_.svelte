0c12da325cc1254183fc2183aa871f0bcfb63b0f
Annie Fu <annie.fu@smartwires.com>
2021-05-14 16:12:39 -0400

    tent cities

---
+++ src/pages/articles/tent-cities-bp/Preview_.svelte
@@ -0,0 +1,19 @@
+<script>
+    // import thumb from './_images/thumb.png'; // NEED A THUMB PNG
+  
+    export let preview;
+  </script>
+  
+  <style>
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  </style>
+  
+  {#if preview}
+    <!-- <img src={thumb} alt="North Carolina Tent City Timeline and Line Graph"> -->
+  {/if}
+  
+  
---
+++ src/pages/articles/tent-cities-bp/index.svelte
@@ -0,0 +1,54 @@
+<!-- routify:options frontmatter= { 
+  "title": "The Effects of Climate Change on Migratory Birds",
+  "description": "Meet the Wood Thrush. Smaller than a Robin, the Wood Thrush makes its home throughout the forests of the Eastern United States. Due to climate change, in recent years, the species has been forced to settle further North than ever before.",
+  "authors": [ "Annie Fu", "Jennifer Russell", "Daniel Sanderson" ],
+  "date": "Dec. 18 2020"
+} -->
+  
+  
+  <div id="header">
+    <h1> *TBD: Tracking Inflow, Outflow, and Events of Charlotte's Tent City</h1>
+    <h2> <i>by Annie Fu and Dana Gong</i></h2>
+  </div>
+
+  <p class="intro-p"> <br/>In Charlotte, North Carolina, the chasm between the rich and the poor is so wide, the city has historically been coined a “tale of two cities:” one of prosperity and the other of poverty. 
+     <br/><br/>
+     The most visible effect of this social stratification is the <strong>“Tent City,”</strong> a cluster of homeless encampments located at the northern end of Uptown 
+     on North College, West 12th, and North Tryon streets.<br/>
+     <br/>
+  <!-- <img src="images/tentcity-map5.png" width="100%" alt="A map of Charlotte's city boundaries, with a bright orange marker in the top right corner."> -->
+  <iframe src="https://snazzymaps.com/embed/303447" width="100%" height="400px" style="border:none;"></iframe>  
+  <br/><br/>
+
+
+In the shadow of the city’s great banking towers and rapid economic growth, 
+rent burdened individuals often fail to escape eviction and homelessness. This pattern can be seen as far back as Charlotte's 1960's urban renewal, 
+when the city government conducted a comprehensive tear-down of low-cost housing occupied mostly by black citizens, and then failed to replace much of it. <br/><br/>
+Charlotte has yet to outpace this history. In 2014, 
+    <a href="https://www.leadingonopportunity.org/" target="_blank">a Ghetty study</a> ranked Charlotte last out of the 50 largest US cities for upward mobility.
+     With the additional onset of Covid-19, Charlotte's low-income citizens have experienced <a href="https://spectrumlocalnews.com/nc/charlotte/news/2020/05/15/special-report--covid-19-s-impact-on-homelessness" target="_blank">increased unemployment and rent burden</a>,
+      two of the largest contributing factors rates of urban homelessness.
+      <br/><br/>
+    Mostly recently, on <strong>February 19th, 2021</strong>, Mecklenburg County officials issued an abatement order that gave Tent City residents 72 hours to 
+    pack all their belongings and leave their encampments. <br/><br/>
+    <!-- In response to the announcement, grassroots organizations and community nonprofit volunteer efforts
+    identified temporary housing for over <strong>210 Tent City residents</strong> in city hotels and emergency shelters for up to 90 days.
+  <br/>><br/>>While a long-term solution is not yet in place, for the time being, relocated individuals have access to three meals a day,
+    job supports, and mental health supports. -->
+    
+     </p>
+
+    
+<p class="intro-p">To get a better sense of how the progression of the pandemic affected Charlotte's homeless population, we looked into the recent events involving the Tent City 
+  encampment, in order to contexualize Charlotte Mecklenburg's raw population counts of Charlotte's 
+  chronically homeless (many of whom resided in Tent City). </p>
+  <!-- <p class="intro-p">
+  </p> -->
+
+  <h1 class="time-header"> Timeline</h1>
+<div class="timeline">
+  <svg id="tri_timeline" width="5580" height="400"></svg>
+  <!-- <script src="timeline.js"></script> -->
+</div>
+  <svg id="tri_graph" width="1300" height="470"></svg>
+  <script src="tri_graph.js"></script>
