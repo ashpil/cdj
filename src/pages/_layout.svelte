5e1b080141d6293582f84f78246585bf43cb8d03
ashpil <shpilenok2001@gmail.com>
2021-09-30 15:00:47 -0400

    updates banner

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -33,7 +33,7 @@
 
 <div id="banner">
     <h6>
-        Hey, you! 👋 Join our <b><a href="https://join.slack.com/t/cornell-data-journal/shared_invite/zt-vrepfq9g-7KPtzv2myUAsTU8YG5mPNg">slack workspace</a></b> to get updates on exciting club details! Our first g-body is <b>Sep. 15, 5-6pm in Mallot 228</b>.
+        Hey, you! 👋 Join our <b><a href="https://join.slack.com/t/cornell-data-journal/shared_invite/zt-vrepfq9g-7KPtzv2myUAsTU8YG5mPNg">slack workspace</a></b> to get updates on exciting club details! Our brainstorming g-body is <b>Oct. 6, 5-6pm in Mallot 228</b>.
     </h6>
 </div>
 <main>
